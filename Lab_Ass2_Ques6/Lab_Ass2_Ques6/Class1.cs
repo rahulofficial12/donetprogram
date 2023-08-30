89a5af1b39686e56a97a8ff9685bb4a216c9779f
rahulofficial12 <142918021+rahulofficial12@users.noreply.github.com>
2023-08-30 08:44:11 +0530

    Add files via upload

---
+++ Lab_Ass2_Ques6/Lab_Ass2_Ques6/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Ass2_Ques6
+{
+    internal partial class Employee
+    {
+        public void Show()
+        {
+            Console.WriteLine("Name of Employee is : " + Name);
+            Console.WriteLine("Salary = " + (BasicSal + 0.10*BasicSal + 0.05*BasicSal));
+        }
+    }
+}
