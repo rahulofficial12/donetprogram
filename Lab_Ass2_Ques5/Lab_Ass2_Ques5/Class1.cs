3d6193ad21b0c0de9173117e157fae241bb48b59
rahulofficial12 <142918021+rahulofficial12@users.noreply.github.com>
2023-08-30 08:42:02 +0530

    Add files via upload

---
+++ Lab_Ass2_Ques5/Lab_Ass2_Ques5/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Ass2_Ques5
+{
+    internal partial class Person
+    {   
+        public void Show()
+        {
+            Console.WriteLine("The Full Name of Person is : " + first + " " + last);
+        }
+    }
+}
